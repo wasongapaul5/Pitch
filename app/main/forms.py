2fc318bb2e3114d3de301cae4bb4d71cfc85676e
wasongapaul5 <wasongapaul5@gmail.com>
2019-11-23 13:04:26 +0300

    import login_user from flask login modul that will create functions of loging in

---
+++ app/main/forms.py
@@ -0,0 +1,11 @@
+# from flask_wtf import FlaskForm
+# from wtforms import StringField,SelectField,TextAreaField,SubmitField
+# from wtforms.validators import Required
+
+# class PostForm(FlaskForm):
+#     title = StringField('Title',validators=[Required()])
+#     post = TextAreaField('Pitch',validators=[Required()])
+#     category = SelectField('Category',choices=[('product', 'product'), ('idea', 'idea'), ('business', 'business')],validators=[Required()])
+#     submit = SubmitField('Post')
+
+# c
