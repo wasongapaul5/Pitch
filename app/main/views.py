526a3752007ed85d10b9de7cf9d01346b56d2a23
wasongapaul5 <wasongapaul5@gmail.com>
2019-11-23 13:27:28 +0300

    add routing functions

--- app/main/views.py
+++ app/main/views.py
@@ -3,3 +3,14 @@ from flask_login import login_required,current_user
 from . import main
 from .forms import PostForm,CommentForm,UpdateProfile
 from ..models import Post,Comment,User,Upvote,Downvote
+
+@main.route('/')
+def index():
+    posts = Post.query.all()
+    product = Post.query.filter_by(category='product').all()
+    idea = Post.query.filter_by(category='idea').all()
+    business = Post.query.filter_by(category='Business').all()
+    return render_template('index.html',business=business,product=product,idea=idea,posts=posts)
+
+
+@main.route('/posts')
