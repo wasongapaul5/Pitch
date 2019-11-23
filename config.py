3c6065fd2dae9850dde0dc1dd3f09cd2e3661c17
wasongapaul5 <wasongapaul5@gmail.com>
2019-11-23 21:01:52 +0300

    add more configurations

--- config.py
+++ config.py
@@ -7,9 +7,19 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = True
 
 
+    ##email config
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USER_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
+    ## simplemde configs
+    SIMPLEMDE_JS = True
+    SIMPLEMDE_USE_CDN = True
 
+class prodConfig(Config):
+    pass
 
-
-
-  #SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:12345@localhost/pitch'
+# class TestConfig(Config):
+#     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:12345@localhost/pitch'
