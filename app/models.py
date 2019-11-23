d5c7e6d86e10d32f2fe9e4f8000541882973e4bc
wasongapaul5 <wasongapaul5@gmail.com>
2019-11-23 14:51:49 +0300

    add a function that will save the user to the db

---
+++ app/models.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+from flask_login import UserMixin,current_user
+from werkzeug.security import generate_password_hash,check_password_hash
+from app import db,login_manager
+
+
+      #user authentication
+class User(UserMixin,db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255),nullable=False,unique=True)
+    email = db.Column(db.String(255),nullable=False,unique=True)
+    password = db.Column(db.String(255),nullable=False)
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
---
+++ manage.py
@@ -0,0 +1,28 @@
+from flask_migrate import Migrate,MigrateCommand
+from flask_script import Manager,Server
+from app import create_app
+from app import db
+from app.models import User,Post,comment
+
+app = create_app(app)
+
+manager = Manager(app)
+migrate = Migrate(app,db)
+
+
+manager.add_command('server',Server(use_debugger=True))
+manager.add_command('db',MigrateCommand)
+
+@manager.shell
+def add_shell_context():
+    return {'db' : db, 'User': User, 'Post':Post, 'Comment':Comment}
+
+@manager.command
+def test():
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+
+if __name__ == '__main__':
+    manager.run()
