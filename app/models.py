a734a9b04fcf8f4515d1b6e350ab2a8f534f838a
wasongapaul5 <wasongapaul5@gmail.com>
2019-11-23 20:27:34 +0300

    add test instances that will test if the app works

--- app/models.py
+++ app/models.py
@@ -108,3 +108,36 @@ class Upvote(db.Model):
     
     def __repr__(self):
         return f'{self.user_id}:{self.post_id}'
+
+
+class DownVote(db.Model):
+    __tablename__ = 'downvotes'
+    id = db.Column(db.Integer,primary_key=True)
+    downvote = db.Column(db,Integer,default=1)
+    user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
+    post_id = db.Column(db.Integer,db.ForeignKey('posts.id'))
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def downvote(cls,id):
+        downvote_post = DownVote(user=current_user,post_id=id)
+        downvote_post.save()
+
+
+    @classmethod
+    def all_downvotes(cls):
+        downvote = Downvote.query.order_by('id').all()
+        return downvote
+
+    def __repr__(self):
+        return f'{self.user_id}:{self.post_id}'
+
+    @classmethod
+    def query_downvotes(cls,id):
+        downvote = DownVote.query.filter_by(post_id=id.all()
+        return downvote
+
+
+
---
+++ test/app_test.py
@@ -0,0 +1,20 @@
+import unittest
+from app.models import User,Post,Comment
+
+class PitchTest(unittest.TestCase):
+    def setUp(self):
+        self.new_user = User(username='moringa', email='wasongapaul5@gmail.com', password='12345')
+        self.new_post = Post()
+        self.new_comment = Comment()
+
+    def test_user_instance(self):
+        pass
+
+    def test_post_instance(self):
+        pass
+
+    def test_comment_instance(self):
+        pass
+
+if __name__ =='__main__':
+    unittest.main()
