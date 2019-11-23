2c1a5ffd85edc3a5419fe6f69279bc14394c8a87
wasongapaul5 <wasongapaul5@gmail.com>
2019-11-23 13:37:37 +0300

    add routing function that gets all the new posts

--- app/main/views.py
+++ app/main/views.py
@@ -21,4 +21,18 @@ def posts():
     user = current_user
     return render_template('pitch_display.html',posts=posts,likes=likes,user=user)
 
+@main.route('/new_post',methods=['GET','POST'])
+@login_required
+def new_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        title = form.title.data
+        post = form.post.data
+        category = form.category.data
+        user_id = current_user._get_current_object().id
+        post_obj = Post(post=post, title=title,category=category,user_id=user_id)
+        post_obj.save()
+        return redirect(url_for('main.index'))
+    return render_template('pitch.html',form=form)
 
+    
